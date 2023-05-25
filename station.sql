6c504cf2578c47476c714221e0c7cd36144044ce
CYJ <jun940317@gmail.com>
2023-05-25 18:31:56 +0900

    20230525 sql works+ VO3+mapper~ing

--- station.sql
+++ station.sql
@@ -7,15 +7,18 @@ CREATE TABLE station (
     station_name VARCHAR(100) PRIMARY KEY,
     station_num INT,
     FOREIGN KEY (station_num)
-        REFERENCES subway (station_num)
+        REFERENCES subwayline2 (station_num)
         ON DELETE CASCADE,
-    exit_count INT NOT NULL,
+    exit_count INT ,
     watercloset_inout BOOLEAN DEFAULT TRUE, #true = inside
     open_doorside BOOLEAN DEFAULT TRUE, #true = left
-    cross_platform INT NOT NULL, #자력 출입가능 2, 환승역통해 가능 1, 불가능 0
-    first_subway_time DATETIME NOT NULL,
-    last_subway_time DATETIME NOT NULL
+    cross_platform INT , #자력 출입가능 2, 환승역통해 가능 1, 불가능 0
+    first_subway_time DATETIME ,
+    last_subway_time DATETIME 
 );
 
+
+SELECT * FROM station
+WHERE exit_count = 1 AND WHERE cross_paltform =1;
 
 DROP TABLE station;
--- subway.sql
+++ subway.sql
@@ -3,7 +3,7 @@ use jspsubway; #DB Name
 SELECT  * FROM subway;
 
 #2호선 (역번호, 환승라인1, 환승라인2)
-CREATE TABLE subway (
+CREATE TABLE subwayline2 (
     station_num INT PRIMARY KEY,
     transfer_line1 INT,
     transfer_line2 INT
