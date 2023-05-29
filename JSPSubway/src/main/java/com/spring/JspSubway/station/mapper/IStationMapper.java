526bf435d75dfeca391c36b4fc4560d4ce0085d6
CYJ <jun940317@gmail.com>
2023-05-29 19:58:00 +0900

    20230529 mapper modi

--- JSPSubway/src/main/java/com/spring/JspSubway/station/mapper/IStationMapper.java
+++ JSPSubway/src/main/java/com/spring/JspSubway/station/mapper/IStationMapper.java
@@ -1,13 +1,13 @@
 package com.spring.JspSubway.station.mapper;
 
-import java.util.List;
-
 import com.spring.JspSubway.command.StationVO;
 
 public interface IStationMapper {
 	
 	//역정보 가져오기
 	StationVO getStationInfo(int sno);
+	
+	//
 
 	//다른역보기
 	void otherStation(int sno);
