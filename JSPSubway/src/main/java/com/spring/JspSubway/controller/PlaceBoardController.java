53c3da42db6660211c357d87402724a9978bee01
kkang3617 <rkdgus3617@naver.com>
2023-06-02 19:14:10 +0900

    230602 머지전 커밋

--- JSPSubway/src/main/java/com/spring/JspSubway/controller/PlaceBoardController.java
+++ JSPSubway/src/main/java/com/spring/JspSubway/controller/PlaceBoardController.java
@@ -2,7 +2,6 @@ package com.spring.JspSubway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
